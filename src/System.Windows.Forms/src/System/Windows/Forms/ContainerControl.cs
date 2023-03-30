fff756c1a619fcb47da946b679aaa1383d8d882e
tombam <bambuch@emclient.com>
2023-03-30 16:31:22 +0200

    change the order in ScaleContainerForDpi(). SetWindowPos may move the form to another screen and thus cause another WM_DPICHANGE which in turn calls another ScaleContainerForDpi, so it must be called at the end, otherwise we get a wrong Font

--- src/System.Windows.Forms/src/System/Windows/Forms/ContainerControl.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ContainerControl.cs
@@ -1448,17 +1448,6 @@ namespace System.Windows.Forms
                 // WM_DPICHANGED event. Failing to apply SuggestedRectangle will result in a circular WM_DPICHANGED
                 // events on the control.
 
-                // Note: SuggestedRectangle supplied  by WM_DPICHANGED event is Dpi (not Font) scaled. if top-level window is
-                // Font scaled, we might see deviations in the expected bounds and may result in adding Scrollbars (horizontal/vertical)
-                User32.SetWindowPos(
-                    new HandleRef(this, HandleInternal),
-                    User32.HWND_TOP,
-                    suggestedRectangle.X,
-                    suggestedRectangle.Y,
-                    suggestedRectangle.Width,
-                    suggestedRectangle.Height,
-                    User32.SWP.NOZORDER | User32.SWP.NOACTIVATE);
-
                 // Bounds are already scaled for the top-level window. We would need to skip scaling of
                 // this control further by the 'OnFontChanged' event.
                 _isScaledByDpiChangedEvent = true;
@@ -1486,6 +1475,17 @@ namespace System.Windows.Forms
                         OnFontChanged(EventArgs.Empty);
                     }
                 }
+
+                // Note: SuggestedRectangle supplied  by WM_DPICHANGED event is Dpi (not Font) scaled. if top-level window is
+                // Font scaled, we might see deviations in the expected bounds and may result in adding Scrollbars (horizontal/vertical)
+                User32.SetWindowPos(
+                    new HandleRef(this, HandleInternal),
+                    User32.HWND_TOP,
+                    suggestedRectangle.X,
+                    suggestedRectangle.Y,
+                    suggestedRectangle.Width,
+                    suggestedRectangle.Height,
+                    User32.SWP.NOZORDER | User32.SWP.NOACTIVATE);
             }
             finally
             {
